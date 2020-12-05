1404ceedd645171493be31018f6174f174ca6989
muht <muht2035@gmail.com>
2020-12-05 18:15:51 +0330

    hi

--- src/test/java/ir/sharif/math/bp99_1/snake_and_ladder/Main.java
+++ src/test/java/ir/sharif/math/bp99_1/snake_and_ladder/Main.java
@@ -3,6 +3,6 @@ package ir.sharif.math.bp99_1.snake_and_ladder;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        // muht
+
     }
 }
