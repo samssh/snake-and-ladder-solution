6d7e5fc67b43ea3192df8c9e4b3859accb086a6a
sam <sam.sadat.shokouhi@gmail.com>
2020-12-22 05:25:55 +0330

    some little change

--- src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/logic/LogicalAgent.java
+++ src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/logic/LogicalAgent.java
@@ -4,16 +4,10 @@ import ir.sharif.math.bp99_1.snake_and_ladder.graphic.GraphicalAgent;
 
 public class LogicalAgent {
 
-    private GraphicalAgent graphicalAgent;
+    private final GraphicalAgent graphicalAgent;
+
 
     public LogicalAgent() {
-    }
-
-    public LogicalAgent(GraphicalAgent graphicalAgent) {
-        this.graphicalAgent = graphicalAgent;
-    }
-
-    public void setGraphicalAgent(GraphicalAgent graphicalAgent) {
-        this.graphicalAgent = graphicalAgent;
+        this.graphicalAgent = new GraphicalAgent(this);
     }
 }
