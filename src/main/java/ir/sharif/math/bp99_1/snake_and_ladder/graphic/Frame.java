21af0e5fad177b1cac84b20bc077f1dec100ed0e
sam <sam.sadat.shokouhi@gmail.com>
2020-12-22 05:24:19 +0330

    working on panels

--- src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/graphic/Frame.java
+++ src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/graphic/Frame.java
@@ -1,40 +1,21 @@
 package ir.sharif.math.bp99_1.snake_and_ladder.graphic;
 
-import ir.sharif.math.bp99_1.snake_and_ladder.graphic.panel.MainPanel;
-import ir.sharif.math.bp99_1.snake_and_ladder.model.GameState;
+import ir.sharif.math.bp99_1.snake_and_ladder.util.Config;
 import ir.sharif.math.bp99_1.snake_and_ladder.util.Loop;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Frame extends JFrame {
-
-    private MainPanel m;
     private int fps;
 
-    public Frame(String p1 , String p2) {              // this constructor is just for test
+    public Frame() {
         this.config();
         new Loop(fps, this::update).start();
-        initialize(p1,p2);
     }
 
+    private void initialize(String player1, String player2) {
 
-/*    public Frame(GameState gs)  { */
-
-/*    }        */                                               /** this is the main constructor
-                                                            it should get sth like GameState, and then
-                                                            create the graphic part of the project */
-
-
-
-
-    private void initialize(String player1 , String player2) {          /** this func is also need GmaeState , for test i use two string */
-
-        m = new MainPanel(player1, player2, 1024, 1024);        //
-        m.setBounds(0, 0, 1280, 720);
-        setContentPane(m);
-
-//
     }
 
     private void config() {
