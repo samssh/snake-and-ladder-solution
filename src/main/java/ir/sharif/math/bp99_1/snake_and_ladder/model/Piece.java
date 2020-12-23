53f0c83124ff7dde97bd2a88802172270faf9662
muht <muht2035@gmail.com>
2020-12-23 22:59:08 +0330

    Piece constructor

--- src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/model/Piece.java
+++ src/main/java/ir/sharif/math/bp99_1/snake_and_ladder/model/Piece.java
@@ -1,20 +1,15 @@
 package ir.sharif.math.bp99_1.snake_and_ladder.model;
 
 public class Piece {
-    private final String name;
     private Cell currentCell;
     private final Color color;
     private final Player player;
 
-    public Piece(Player player,String name,Color color){
+    public Piece(Player player,Color color){
         this.player = player;
-        this.name = name;
         this.color = color;
     }
 
-    public String getName() {
-        return name;
-    }
 
     public Player getPlayer() {
         return player;
